9ebc5f18c97f2afb9933372229770889017dd541
local9 <apjcookie1@hotmail.com>
2023-04-08 14:03:34 +0100

    tweak: change user load to use a defined order

--- ProjectName.Server/Database/Domain/User.cs
+++ ProjectName.Server/Database/Domain/User.cs
@@ -32,16 +32,35 @@ namespace ProjectName.Server.Database.Domain
                 identifiers.Add(GetPlayerIdentifier(player.Handle, i));
             }
 
-            // find user using tokens
-            User user = await OnGetUserByTokensAsync(tokens);
+            string fivem = identifiers.FirstOrDefault(x => x.StartsWith("fivem:"));
+            string license = identifiers.FirstOrDefault(x => x.StartsWith("license:"));
+            string xbl = identifiers.FirstOrDefault(x => x.StartsWith("xbl:"));
+            string live = identifiers.FirstOrDefault(x => x.StartsWith("live:"));
+            string discord = identifiers.FirstOrDefault(x => x.StartsWith("discord:"));
+            string steam = identifiers.FirstOrDefault(x => x.StartsWith("steam:"));
+
+            // find user using fivem identifier
+            User user = await OnGetUserByIdentityAsync(fivem);
+            if (user is not null) return user;
+            // find user using license identifier
+            user = await OnGetUserByIdentityAsync(license);
+            if (user is not null) return user;
+            // find user using xbl identifier
+            user = await OnGetUserByIdentityAsync(xbl);
+            if (user is not null) return user;
+            // find user using live identifier
+            user = await OnGetUserByIdentityAsync(live);
+            if (user is not null) return user;
+            // find user using discord identifier
+            user = await OnGetUserByIdentityAsync(discord);
+            if (user is not null) return user;
+            // find user using steam identifier
+            user = await OnGetUserByIdentityAsync(steam);
             if (user is not null) return user;
 
-            // find user using identifiers
-            foreach (string identity in identifiers)
-            {
-                user = await OnGetUserByIdentityAsync(identity);
-                if (user is not null) return user;
-            }
+            // find user using tokens
+            user = await OnGetUserByTokensAsync(tokens);
+            if (user is not null) return user;
 
             // if no user is returned, then create one.
             if (user is null)
@@ -92,6 +111,8 @@ namespace ProjectName.Server.Database.Domain
 
         private static async Task<User> OnGetUserByIdentityAsync(string identity)
         {
+            if (string.IsNullOrEmpty(identity)) return null;
+
             string[] ident = identity.Split(':');
             string type = ident[0];
             string value = ident[1];
