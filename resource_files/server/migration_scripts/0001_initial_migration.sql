213cf40d689a5ffb7396fb9d6851ee044ce87d3d
127.0.0.1 <kinglo0py@gmail.com>
2023-06-21 11:48:51 +0100

    tweak: updated migration sql to include some dump information

--- resource_files/server/migration_scripts/0001_initial_migration.sql
+++ resource_files/server/migration_scripts/0001_initial_migration.sql
@@ -1,3 +1,17 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.10.2-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 -- Create a stored procedure to insert a new user into the 'users' table
 CREATE PROCEDURE `insUser`(IN `pUsername` VARCHAR(255)) SQL SECURITY INVOKER BEGIN
     INSERT INTO users (`last_name`)
@@ -43,3 +57,8 @@ CREATE PROCEDURE `selUserByToken`(IN `pTokens` VARCHAR(2000)) SQL SECURITY INVOK
     INNER JOIN users u ON u.id = ut.user_id
     WHERE FIND_IN_SET(ut.token, pTokens);
 END;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
